844ea7001ef5dc04356ff7d5a55c1856faf3a613
sduzzy <3056569021@qq.com>
2023-07-06 21:26:28 +0800

    Update project5.py

--- Project5 Impl Merkle Tree following RFC6962/project5.py
+++ Project5 Impl Merkle Tree following RFC6962/project5.py
@@ -5,13 +5,13 @@ from tkinter import N
 def Create_Merkle_Tree(data):
     Depth=math.ceil(math.log(len(data), 2))+1 #计算深度
     Merkle_Tree=[[] for _ in range(Depth)] #定义一个Merkel_Tree
+    Merkle_Tree[Depth-1] = [(hashlib.sha256("0x00".encode()+i.encode())).hexdigest() for i in data]
     #将数据块的hash值存入叶子节点
     #将每两个子节点相加后哈希值写入父节点
     for n in range(1,Depth):
         i=Depth-1-n
         L = math.floor(len(Merkle_Tree[i+1])/2)
         for j in range(0, L):
-            Merkle_Tree[i].append((hashlib.sha256(Merkle_Tree[i+1][2*j].encode() + Merkle_Tree[i+1][2*j+1].encode())).hexdigest())
             Merkle_Tree[i].append((hashlib.sha256("0x01".encode()+Merkle_Tree[i+1][2*j].encode() + Merkle_Tree[i+1][2*j+1].encode())).hexdigest())
         if len(Merkle_Tree[i+1])%2 == 1:
                Merkle_Tree[i].append(Merkle_Tree[i+1][-1])
